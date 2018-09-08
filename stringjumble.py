0604eb02412932c4320956d0e2d7ff6ef2807543
Npikielny <42814781+Npikielny@users.noreply.github.com>
2018-09-07 23:04:05 -0400

    Update stringjumble.py

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,15 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+string = str(input("Please enter a string of text (the bigger the better): "))
+stringList = list(i for i in string)
+wordList = []
+print(list(stringList[len(stringList)::-1]))
+newWord = 0
+for i in range(1, len(string) + 1):
+    if i == " ":
+        newWord = 1
+    elif newWord == 1:
+        wordList[i] += string[i]
+    else:
+        wordList[i] = string[i]
